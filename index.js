08b78a2f84487f0431515c059f08662feccad010
Haoming Yu <yu2974201195@gmail.com>
2024-12-06 17:54:58 -0600

    make it better

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 4000;
 
 
 app.use(cors({
-    origin: 'https://RicebookHaoming.surge.sh/', // Exact frontend origin
+    origin: 'https://social-app-ricebook.surge.sh/', // Exact frontend origin
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
